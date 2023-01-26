60bc7d5c80feccdaa9b5e53a354cc53a7c5a3f49
miyoung <noonchicat@gmail.com>
2023-01-26 22:25:16 +0900

    feat:공유버튼 생성

--- src/components/ExpenseMain.jsx
+++ src/components/ExpenseMain.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { ShareFill } from "react-bootstrap-icons";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import { ServiceLogo } from "./shared/ServiceLogo";
@@ -13,6 +14,21 @@ import { useGroupData } from "../hooks/useGroupData";
 
 const ExpenseMain = () => {
   useGroupData();
+
+  const handleSharing = () => {
+    if (navigator.userAgent.match(/iphone|android/i) && navigator.share) {
+      navigator.share({
+        url: window.location.href,
+      });
+    } else {
+      navigator.clipboard.writeText(window.location.href).then(() => {
+        alert(
+          "공유 링크가 클립 보드에 복사 되었습니다. 그룹 멤버들과 공유해 보세요!"
+        );
+      });
+    }
+  };
+
   return (
     <Container fluid>
       <Row>
@@ -23,6 +39,10 @@ const ExpenseMain = () => {
           <RightPane />
         </Col>
       </Row>
+
+      <StyledShareButton onClick={handleSharing}>
+        <ShareFill />
+      </StyledShareButton>
     </Container>
   );
 };
@@ -60,6 +80,24 @@ const RightPane = () => {
     </StyledRightPaneContainer>
   );
 };
+
+const StyledShareButton = styled.div`
+  border-radius: 50%;
+  background-color: #6b3da6;
+  position: fixed;
+  width: 55px;
+  height: 55px;
+  right: 40px;
+  bottom: 45px;
+  filter: drop-shadow(4px 4px 6px rgba(0, 0, 0, 0.25));
+  color: white;
+  font-size: 30px;
+  text-align: center;
+  svg {
+    padding-right: 3px;
+    padding-top: 3px;
+  }
+`;
 
 const StyledRightPaneContainer = styled(Container)`
   padding: 100px 31px;
